0a0cc1681b5c7867b99de8344ea952bf3f1fe99e
Krzysztof Zabłocki <krzysztof.zablocki@pixle.pl>
2024-12-19 13:57:30 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "AutomaticSettings",
     platforms: [
             .macOS(.v10_15),
-            .iOS(.v13)
+            .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
